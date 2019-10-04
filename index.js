26cb7847c2776dbf3d1773748921b7c374c393dd
jbc5099 <jbc5099@gmail.com>
2019-10-04 14:59:09 -0600

    added pagnated query for all questions

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const { db, Question } = require('./db');
 const app = express();
 
 app.listen(3000, () => console.log('listening on port 3000'));
+
+app.get('/questions', (req, res) => {
+  const pageOptions = {
+    page: req.query.page || 0,
+    limit: req.query.limit || 10
+  }
+
+Question.find()
+    .skip(pageOptions.page*pageOptions.limit)
+    .limit(pageOptions.limit)
+    .exec((err, doc) => {
+        if(err) { res.status(500).json(err); return; };
+        res.status(200).json(doc);
+    })
+})
